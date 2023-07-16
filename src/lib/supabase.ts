193eecc3f1c4893e273ea01232f69c7956401970
oljasa <oljasalishev@gmail.com>
2023-07-15 20:08:35 -0400

    changes

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,8 +1,8 @@
 // /lib/supabase.ts
 
 import { createClient } from '@supabase/supabase-js'
-console.log(import.meta.env.VITE_SUPABASE_URL)
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
+console.log(import.meta.env.VITE_PUBLIC_SUPABASE_URL)
+const supabaseUrl = import.meta.env.VITE_PUBLIC_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_PUBLIC_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
